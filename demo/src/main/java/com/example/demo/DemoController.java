2e07e30cdf8cc73f5248be9d74725c0bc104fd00
Cheolkie Kim <ckiekim@naver.com>
2023-08-08 16:04:29 +0900

    Spring

--- demo/src/main/java/com/example/demo/DemoController.java
+++ demo/src/main/java/com/example/demo/DemoController.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class DemoController {
@@ -23,5 +26,17 @@ public class DemoController {
 	@GetMapping("/login")
 	public String login() {
 		return "02.login";
+	}
+	
+	@PostMapping("/login") 
+//	@ResponseBody
+	public String loginProc(HttpServletRequest req, Model model) {
+		String uid = req.getParameter("uid");		// "uid" 가 input tag의 name 값
+		String pwd = req.getParameter("pwd");
+//		return "<h1>uid=" + uid + ", pwd=" + pwd + "</h1>";
+		
+		model.addAttribute("uid", uid);
+		model.addAttribute("pwd", pwd);
+		return "03.loginResult";
 	}
 }
