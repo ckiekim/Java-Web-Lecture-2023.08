ddaf1365869a0d7f1721e3708524d1ddd366dcb8
Cheolkie Kim <ckiekim@naver.com>
2023-08-14 15:57:47 +0900

    회원 등록

--- demo/src/main/java/com/example/demo/BootstrapController.java
+++ demo/src/main/java/com/example/demo/BootstrapController.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("/bs")
 public class BootstrapController {
+	@Value("${spring.servlet.multipart.location}") private String uploadDir;
 
 	@GetMapping("/login")
 	public String login() {
@@ -31,6 +37,37 @@ public class BootstrapController {
 	@ResponseBody
 	public String login2(String email, String pswd, String remember) {
 		return "<h2>email: " + email + ", pswd: " + pswd + ", remember: " + remember + "</h2>";
+	}
+	
+	@GetMapping("/register")
+	public String registerForm() {
+		return "bs/register";
+	}
+	
+	@PostMapping("/register")
+	@ResponseBody
+	public String registerProc(MultipartHttpServletRequest req) {
+		String uid = req.getParameter("uid");
+		String pwd = req.getParameter("pwd");
+		String pwd2 = req.getParameter("pwd2");
+		String uname = req.getParameter("uname");
+		String email = req.getParameter("email");
+		String gender = req.getParameter("gender");
+		String html = String.format("<h2>%s, %s, %s, %s, %s, %s</h2>", uid, pwd, pwd2, uname, email, gender);
+		
+		// 파일 읽기
+		MultipartFile filePart = req.getFile("profile");
+		if (filePart.getContentType().contains("image")) {	// 사진을 보냈음
+			String filename = filePart.getOriginalFilename();
+			html += "<h2>" + filename + "</h2>";
+			String profilePath = uploadDir + filename;
+			try {
+				filePart.transferTo(new File(profilePath));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return html;
 	}
 	
 }
