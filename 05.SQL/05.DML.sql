dcbb4cc7036f65bd27352115392a25d234690b46
Cheolkie Kim <ckiekim@naver.com>
2023-08-21 15:19:14 +0900

    SQL

--- 05.SQL/05.DML.sql
+++ 05.SQL/05.DML.sql
@@ -22,4 +22,46 @@ insert into orders
 -- 박찬호 고객이 스포츠 심리라는 책을 오늘 구매한 사실을 Orders 테이블에 기록
 insert into orders(orderid, saleprice, custid, bookid)
     values(12, 24000, 1, 12);
+-- 고객 테이블에 신유빈 선수를 등록
+insert into customer(custid, name) values(6, '신유빈');
+commit;
 select * from orders;
+
+/*
+ * 2.2 갱신(Update)
+
+UPDATE 테이블명
+    SET 필드명1=값1, ..., 필드명n=값n
+    WHERE 조건;
+
+ */
+-- 스포츠 심리 책의 출판사를 한솔의학서적으로 변경
+update book set publisher='한솔의학서적' where bookid=12;
+select * from book;
+-- 신유빈 선수의 주소를 강원도 영월, 전화번호를 010-2345-6789 으로 변경
+update customer set address='강원도 영월', phone='010-2345-6789'
+    where custid=6;
+-- 전화번호가 null인 고객을 070-2345-6789로 변경
+update customer set phone='070-2345-6789' where phone is null;
+select * from customer;
+commit;
+
+/*
+ * 2.3 삭제(DELETE)
+ 
+DELETE FROM 테이블명 WHERE 조건;
+
+*/
+-- 고객 테이블에 테스트 데이터 입력후 삭제
+insert into customer(custid, name) values (7, '테스트');
+insert all
+    into customer(custid, name) values(8, '류현진')
+    into customer(custid, name) values(9, '손흥민')
+    select * from dual;
+select * from customer;
+-- custid가 7인 고객 삭제
+delete from customer where custid=7;
+-- 주소가 null인 고객 삭제
+delete from customer where address is null;
+select * from customer;
+commit;
--- 05.SQL/99.연습문제.sql
+++ 05.SQL/99.연습문제.sql
@@ -27,3 +27,41 @@ select orderid from orders where orderdate not between '2014-07-04' and '2014-07
 select name, address from customer where name like '김%';
 -- (7) 성이 ‘김’ 씨이고 이름이 ‘아’로 끝나는 고객의 이름과 주소
 select name, address from customer where name like '김%아';
+
+/* 3. 마당서점에서 다음의 심화된 질문에 대해 SQL 문을 작성하시오. */
+-- (1) 박지성이 구매한 도서의 출판사와 같은 출판사에서 도서를 구매한 고객의 이름
+select b.publisher from orders o
+    join book b on o.bookid=b.bookid
+    join customer c on o.custid=c.custid
+    where c.name like '박지성';
+select distinct ec.name from orders eo
+    join book eb on eo.bookid=eb.bookid
+    join customer ec on eo.custid=ec.custid
+    where eb.publisher in (select b.publisher from orders o
+        join book b on o.bookid=b.bookid
+        join customer c on o.custid=c.custid
+        where c.name like '박지성');
+-- (2) 두 개 이상의 서로 다른 출판사에서 도서를 구매한 고객의 이름
+select c.name, count(DISTINCT b.publisher) from orders o
+    join book b on o.bookid=b.bookid
+    join customer c on o.custid=c.custid
+    group by c.name having count(DISTINCT b.publisher)>=2;
+-- (3) (생략) 전체 고객의 30% 이상이 구매한 도서 
+select b.bookname, count(o.bookid) from orders o
+    join book b on o.bookid=b.bookid
+    group by b.bookname having count(o.bookid) >= 2;
+select * from orders;
+
+/* 다음 질의에 대해 DML 문을 작성하시오. */
+-- (1) 새로운 도서 ('스포츠 세계', '대한미디어', 10000원)이 마당서점에 입고되었다. 
+--     삽입이 안 될 경우 필요한 데이터가 더 있는지 찾아보자.
+insert into book values(13, '스포츠 세계', '대한미디어', 10000);
+-- (2) '삼성당'에서 출판한 도서를 삭제해야 한다.
+delete from book where publisher like '삼성당';
+select * from book;
+-- (3) '이상미디어'에서 출판한 도서를 삭제해야 한다. 삭제가 안 될 경우 원인을 생각해보자.
+select * from orders
+    where bookid in (select bookid from book where publisher like '이상미디어');
+-- (4) 출판사 '대한미디어'가 '대한출판사'로 이름을 바꾸었다.
+update book set publisher='대한출판사'
+    where publisher like '대한미디어';
