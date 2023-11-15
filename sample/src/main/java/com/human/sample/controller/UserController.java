0c9806704f6b2d77b6f84d8ddb5c71f2b2835468
Cheolkie Kim <ckiekim@naver.com>
2023-11-15 12:41:53 +0900

    Sample

---
+++ sample/src/main/java/com/human/sample/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.human.sample.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping("/register")
+	public String register() {
+		return "user/register";
+	}
+	
+}
