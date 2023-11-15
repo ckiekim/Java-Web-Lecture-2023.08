d3268506e57441cf1bd50bfaeb0c8c411d6c08c0
Cheolkie Kim <ckiekim@naver.com>
2023-11-15 13:34:55 +0900

    Sample

--- sample/src/main/java/com/human/sample/controller/UserController.java
+++ sample/src/main/java/com/human/sample/controller/UserController.java
@@ -3,7 +3,10 @@ package com.human.sample.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.human.sample.entity.User;
 
 @Controller
 @RequestMapping("/user")
@@ -12,6 +15,13 @@ public class UserController {
 	@GetMapping("/register")
 	public String register() {
 		return "user/register";
+	}
+	
+	@PostMapping("/register")
+	public String registerProc(String uid, String pwd, String pwd2, String uname, String email) {
+		User u = new User(uid, pwd, uname, email);
+		System.out.println(u);
+		return "redirect:/home";
 	}
 	
 }
