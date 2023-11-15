df64b79d7f22cb666239ddaae490a6d190fac48a
Cheolkie Kim <ckiekim@naver.com>
2023-11-15 11:49:07 +0900

    Sample

---
+++ sample/src/test/java/com/human/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.human.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
